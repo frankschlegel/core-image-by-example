3d500a5ae42081080876176084b2458ffe631c4b
Frank Schlegel <frank.schlegel@digitalmasterpieces.com>
2020-07-13 19:45:07 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Frank Schlegel on 13.07.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Core_Image_by_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_Image_by_ExampleApp.swift
+//  Shared
+//
+//  Created by Frank Schlegel on 13.07.20.
+//
+
+import SwiftUI
+
+@main
+struct Core_Image_by_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
